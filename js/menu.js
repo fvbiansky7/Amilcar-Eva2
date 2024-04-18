489a41b72ca7c78a00730f8e5140480998d9bfde
Amilcar <amilcarcontreras98@gmail.com>
2024-04-18 19:07:46 -0400

    Añadí el carrusel

--- js/menu.js
+++ js/menu.js
@@ -5,3 +5,10 @@ btnMenu.addEventListener('click',function(){
     menu.classList.toggle('mostrar');
  
 });
+
+// const myCarouselElement = document.querySelector('#myCarousel')    este es el constructor del carrusel pero de momento no lo necesito
+
+// const carousel = new bootstrap.Carousel(myCarouselElement, {
+//   interval: 2000,
+//   touch: false
+// })
